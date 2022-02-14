5b2b40d2f65423d6a33def66ce81dbe42ecacb04
Haohao-555 <3283627490@qq.com>
2022-02-14 20:08:04 +0800

    环形图

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,24 @@
 -->
 <template>
   <div id="app">
-    <!-- 环形图 -->
-    <circular></circular>
-    <!-- 仪表图 -->
-    <gauge></gauge>
-    <!-- 曲线图 -->
-    <spline></spline>
-    <!-- 柱状图 -->
-    <column></column>
-    <!-- 横向柱状图 -->
-    <bar></bar>
-    <!-- 散点图 -->
-    <scatter></scatter>
+    <div class="main">
+      <div class="h-conatiner">
+        <!-- 环形图 -->
+        <circular></circular>
+        <!-- 仪表图 -->
+        <gauge></gauge>
+        <!-- 曲线图 -->
+        <spline></spline>
+        <!-- 柱状图 -->
+        <column></column>
+        <!-- 横向柱状图 -->
+        <bar></bar>
+        <!-- 散点图 -->
+        <scatter></scatter>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 import circular from "./components/Circular.vue";
 import gauge from "./components/Gauge.vue";
@@ -36,10 +39,108 @@ export default {
     spline,
     column,
     bar,
-    scatter
+    scatter,
+  },
+  data() {
+    return {
+      realNum: 0,
+    };
+  },
+  mounted() {
+    this.initLayout();
+  },
+  methods: {
+    initLayout() {
+      window.addEventListener("load", () => {
+        this.realNum = document.querySelectorAll(".h-conatiner .chart").length;
+        this.dealContainer();
+      });
+      window.addEventListener("resize", () => {
+        this.dealContainer();
+      });
+    },
+    dealContainer() {
+      let cardList = document.querySelectorAll(".h-conatiner .chart");
+      for (let i = this.realNum; i < cardList.length; i++) {
+        cardList[i].remove();
+      }
+      let distance = cardList[0].offsetLeft;
+      let w =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+      let cardWidth = cardList[0].clientWidth + 2 * distance;
+
+      let num = (w / cardWidth).toFixed(1);
+
+      let last = this.realNum % num;
+
+      for (let j = last == 0 ? num : last; j < num; j++) {
+        let card = document.createElement("div");
+        card.className = "chart";
+        document.querySelector(".h-conatiner").appendChild(card);
+      }
+    },
   },
 };
 </script>
 
 <style>
+
+.btn-active {
+  border-color: #00428b !important;
+  color: #00428b !important;
+}
+.el-button-group > .el-button:first-child {
+  border-top-right-radius: 2px !important;
+  border-bottom-right-radius: 2px !important;
+}
+.el-button-group > .el-button:last-child {
+  border-top-left-radius: 2px !important;
+  border-bottom-left-radius: 2px !important;
+}
+#app {
+  color: #000;
+  font-size: 12px;
+  min-height: 100vh;
+  min-width: 800px;
+  background: #f3f3f3;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.main {
+  display: block;
+  flex: 1;
+  flex-basis: auto;
+  overflow: auto;
+}
+.h-conatiner {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  overflow-x: hidden;
+}
+.chart {
+  width: 340px;
+  height: 340px;
+  margin-bottom: 10px;
+  margin-right: 5px;
+  margin-left: 5px;
+}
+.box-card {
+  height: 100%;
+  padding: 10px;
+}
+.el-card__body {
+  padding: 6px 0px !important;
+}
+.el-card__header {
+  padding: 10px 10px 10px 0px !important;
+}
+.chart {
+  position: relative;
+}
 </style>
--- src/components/Circular.vue
+++ src/components/Circular.vue
@@ -4,3 +4,152 @@
  * @LastEditors: 浩
  * @FilePath: \highcharts\src\components\Circular.vue
 -->
+<template>
+  <div class="circular chart">
+    <el-card class="box-card" shadow="always">
+      <div slot="header">
+        <span class="title">终端概况</span>
+      </div>
+      <div class="container">
+        <el-button-group>
+          <el-button
+             size="mini"
+            style="margin-right: 10px"
+            :class="[btn == 0 ? 'btn-active' : '']"
+            @click="btn = 0"
+            >终端统计</el-button
+          >
+          <el-button
+            size="mini"
+            :class="[btn == 1 ? 'btn-active' : '']"
+            @click="btn = 1"
+            >终端合理统计</el-button
+          >
+        </el-button-group>
+        <div id="highcharts-one" style="min-width: 318px; height: 270px"></div>
+      </div>
+    </el-card>
+  </div>
+</template>
+<script>
+import HighCharts from "highcharts";
+import highchartsMore from "highcharts/highcharts-more";
+highchartsMore(HighCharts);
+export default {
+  name: "chartone",
+  data() {
+    return {
+      // 设置数据的阈值
+      min: 300,
+      max: 1000,
+
+      btn: 0,
+      command: 0,
+      // chart 实例
+      chart: null,
+      // 配置选项
+      option: {
+        legend: {
+          symbolRadius: 0,
+          itemMarginBottom: 12
+        },
+        title: {
+          floating: true,
+          useHTML: true,
+          style: {
+            fontSize: "11px",
+            color: "#809CC1",
+            textAlign: "center",
+          },
+        },
+        credits: {
+          enabled: false,
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: false,
+            cursor: "pointer",
+            size: 150,
+            dataLabels: {
+              enabled: true,
+              format: "<b style='font-weight: normal;'>{point.name}:</b><br/>{point.y}",
+              connectorWidth: 1,
+              connectorPadding: 0,
+              crookDistance: "10%",
+              distance: 10,
+            },
+            showInLegend: true,
+          },
+        },
+        series: [
+          {
+            type: "pie",
+            innerSize: "70%",
+            name: "终端数",
+          },
+        ],
+      },
+      // 数据
+      dataList: [],
+      // 终端统计（格式）
+      statistFormat: [
+        { name: "在线终端", y: 0, color: "#63adff" },
+        { name: "离线终端", y: 0, color: "#ccd6e3" },
+        { name: "异常终端", y: 0, color: "#ff5d6a" },
+      ],
+      // 合理终端显示（格式）
+      reasonFormat: [
+        { name: "在线终端", y: 0, color: "#63adff" },
+        { name: "离线终端", y: 0, color: "#ccd6e3" },
+      ],
+    };
+  },
+  watch: {
+    btn() {
+      this.update();
+    },
+  },
+  mounted() {
+    this.update();
+  },
+  methods: {
+    update() {
+      // 判断
+      let num = this.btn == 0 ? 3 : 2;
+      let format = this.btn == 0 ? this.statistFormat : this.reasonFormat;
+      let sum = 0;
+      // 随机生成数据
+      for (let i = 0; i < num; i++) {
+        let newY = this.min + Math.round(Math.random() * (this.max - this.min));
+        format[i].y = newY;
+        sum += newY;
+      }
+      // 设置标题
+      this.option.title.text = `<i class='el-icon-monitor icon'></i></br><b>终端总数：${sum}</b>`;
+
+      this.dataList = format;
+      
+      // 初始化
+      let chart = HighCharts.chart("highcharts-one", this.option, function (c) {
+        c.setTitle({
+          y: c.series[0].center[1] + parseInt(c.title.styles.fontSize) / 2 - 35,
+        });
+      });
+      chart.series[0].setData(this.dataList);
+    },
+    handleCommand(command) {
+      if (command != "end-cmd-2") {
+        this.btn = command;
+      } else {
+        this.$message("菜单项command：" + command);
+      }
+    },
+  },
+};
+</script>
+<style>
+.icon {
+  color: "#809CC1";
+  font-size: 25px;
+}
+</style>
